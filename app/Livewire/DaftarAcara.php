5cd7956f621219371ac32f6503793e107eae92df
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2026-02-16 14:28:35 +0800

    Create submit with validation

--- app/Livewire/DaftarAcara.php
+++ app/Livewire/DaftarAcara.php
@@ -2,6 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Models\Acara;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class DaftarAcara extends Component
@@ -11,6 +15,12 @@ class DaftarAcara extends Component
     public $tarikh;
     public $lokasi;
     public $penganjur;
+    public $slug;
+
+    public function updatedTajuk($value)
+    {
+        $this->slug = Str::slug($value);
+    }
 
     public function render()
     {
@@ -19,6 +29,36 @@ class DaftarAcara extends Component
 
     public function simpanAcara()
     {
-        dd('Simpan Acara dipanggil');
+        $rules = [ 
+                    'tajuk' => 'required',
+                    'keterangan' => 'required',
+                    'tarikh' => 'required|date',
+                    'lokasi' => 'required',
+                    'penganjur' => 'required',
+                    'slug' => 'required|unique:acara,slug',
+        ];
+
+        $customMessages = [
+            'tajuk.required' => 'Sila masukkan tajuk acara.',
+            'keterangan.required' => 'Sila masukkan keterangan acara.',
+            'tarikh.required' => 'Sila masukkan tarikh acara.',
+            'tarikh.date' => 'Tarikh tidak sah.',
+            'lokasi.required' => 'Sila masukkan lokasi acara.',
+            'penganjur.required' => 'Sila masukkan penganjur acara.',
+            'slug.required' => 'Sila masukkan slug acara.',
+            'slug.unique' => 'Slug ini telah digunakan.',
+        ];
+
+        //dd($this->all());
+
+        $validator = Validator::make($this->all(), $rules, $customMessages);
+
+        $validated = $validator->validated();
+        
+        Arr::set($validated, 'waktu', $this->tarikh);
+
+        //dd($validated);
+
+        Acara::create($validated);
     }
 }
