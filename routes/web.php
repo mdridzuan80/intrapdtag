252d91af5b50a1f7a3a6a3387de20e6fec22bb06
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2026-02-23 16:09:15 +0800

    update default route to reroute login
    change logo login and title

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,11 @@ use App\Livewire\InfoAcara;
 use App\Livewire\ListAcara;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
